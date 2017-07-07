0016b550f73268b4e6c42bf7492778e31a727bc7
Pascal Thomet <pthomet@ivsweb.com>
2017-07-07 21:04:12 +0200

    Silence warning / virtual destructor inside bufferpool.hpp

--- modules/core/include/opencv2/core/bufferpool.hpp
+++ modules/core/include/opencv2/core/bufferpool.hpp
@@ -7,6 +7,11 @@
 #ifndef OPENCV_CORE_BUFFER_POOL_HPP
 #define OPENCV_CORE_BUFFER_POOL_HPP
 
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable: 4265) // class has virtual functions, but destructor is not virtual
+#endif
+
 namespace cv
 {
 
@@ -27,5 +32,9 @@ public:
 //! @}
 
 }
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
 
 #endif // OPENCV_CORE_BUFFER_POOL_HPP
